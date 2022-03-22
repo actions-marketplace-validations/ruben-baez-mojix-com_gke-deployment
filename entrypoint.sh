a2cb219402973c8a7133e4a7093c5f13d843b6a0
Ruben <ruben.baez@mojix.com>
2022-03-22 17:23:39 -0500

    fix output message

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,9 +39,7 @@ kubectl rollout restart deploy/$INPUT_DEPLOY -n $INPUT_NAMESPACE
 kubectl get deploy/$INPUT_DEPLOY -o wide -n $INPUT_NAMESPACE
 
 # Capture output
-output=$("$INPUT_DEPLOY deploy updated with: $INPUT_IMAGE")
-current_date=$(date)
-echo "deployment time: $current_date"
+output=$("$INPUT_DEPLOY deploy updated succesfuly..")
 # Preserve output for consumption by downstream actions
 echo "$output" > "${HOME}/${GITHUB_ACTION}.log"
 
